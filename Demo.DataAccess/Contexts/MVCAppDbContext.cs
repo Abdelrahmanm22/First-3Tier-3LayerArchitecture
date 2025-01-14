b10dc4f4c319c09ffd78a1284acc9f1b58d50cec
Abdelrahmanm22 <abdra1396@gmail.com>
2025-01-14 14:38:09 +0200

    code for Data Access Layer: create department model and DbContext and put connection string

---
+++ Demo.DataAccess/Contexts/MVCAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.DataAccess.Contexts
+{
+    internal class MVCAppDbContext:DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = MVCAppDb; Trusted_Connection = True");
+        }
+
+    }
+}
