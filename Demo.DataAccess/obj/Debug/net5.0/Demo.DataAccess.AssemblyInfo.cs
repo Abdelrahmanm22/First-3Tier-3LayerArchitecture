3b1e8e80ee7f6bee678e5d2e2926988cb32aeb53
Abdelrahmanm22 <abdra1396@gmail.com>
2025-01-17 21:09:33 +0200

    Show Imgae of Employee in Employees tabel

--- Demo.DataAccess/obj/Debug/net5.0/Demo.DataAccess.AssemblyInfo.cs
+++ Demo.DataAccess/obj/Debug/net5.0/Demo.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d9ad06a7de5a8c312d17f05a4445e08164588ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bca28bc286c75b4583c9ec4ad74fa9a70dd9ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
