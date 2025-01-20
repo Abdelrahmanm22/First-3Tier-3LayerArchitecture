afbdbd7bc26436e2a86dd52adbede9afe7204fd3
Abdelrahmanm22 <abdra1396@gmail.com>
2025-01-21 01:33:43 +0200

    Microsoft Identity Package install and migration for 7 tabels

--- Demo.DataAccess/Contexts/MVCAppDbContext.cs
+++ Demo.DataAccess/Contexts/MVCAppDbContext.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Demo.DataAccess.Contexts
 {
-    public class MVCAppDbContext:DbContext
+    public class MVCAppDbContext:IdentityDbContext
     {
         public MVCAppDbContext(DbContextOptions<MVCAppDbContext> options):base(options)
         {
@@ -16,6 +18,8 @@ namespace Demo.DataAccess.Contexts
         }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        //public DbSet<IdentityUser> Users { get; set; }
+        //public DbSet<IdentityRole> Roles { get; set; }
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
         //    optionsBuilder.UseSqlServer("Server = .; Database = MVCAppDb; Trusted_Connection = True");
--- Demo.Presentation/obj/Debug/net5.0/Demo.Presentation.AssemblyInfo.cs
+++ Demo.Presentation/obj/Debug/net5.0/Demo.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdace7cf59c16f49232df840ff38470a3940c8b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e887a8ed98282795f9e103116ab7d1d19d0fee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
